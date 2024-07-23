851b9af53eee7c1aa2631c2376e205b8130d5f8f
Michael Sohnen <michaelsohnenacademic@gmail.com>
2024-07-23 13:13:04 -0400

    Further refined link handling

--- lib/tree.py
+++ lib/tree.py
@@ -3,15 +3,16 @@ from typing import Dict, Optional
 
 from termcolor import colored
 
+
 class TreeNode:
     value: Optional[str]
-    children: Dict[str,"TreeNode"]
+    children: Dict[str, "TreeNode"]
 
     def __init__(self, value=None, children=None):
         self.value = value
         self.children = children if children is not None else dict()
 
-    def insert(self,path, value):
+    def insert(self, path, value):
         if len(path) == 0:
             self.value = value
         if len(path) == 1:
@@ -27,22 +28,38 @@ class TreeNode:
         if len(self.children) == 0:
             return str(self.value)
         else:
-            s=""
+            s = ""
             for key in self.children:
-                if len(self.children[key].children)==0:
-                     s+=str(self.children[key])+"\n"
+                if len(self.children[key].children) == 0:
+                    s += str(self.children[key]) + "\n"
                 else:
-                    s+=" "*0+colored(f"{key}:\n","magenta")
-                    sub=f"{str(self.children[key])}"
+                    s += " " * 0 + colored(f"{key}:\n", "magenta")
+                    sub = f"{str(self.children[key])}"
                     for line in sub.split("\n"):
-                        s+=" "*4+f"{line}\n"
+                        s += " " * 4 + f"{line}\n"
             return s.rstrip()
 
 
+def smartsplit(s, sep="/"):
+    if "->" in s:
+        A, B = s.split("->")
+        A = A.strip()
+        B = B.strip()
+        return A.split(sep) + ["-> " + B]
+    return s.split(sep)
+
+
 def filepaths_to_tree(filepaths):
-    filepaths = list(map(lambda p:re.sub(r"((^\/)|(\/$))","",re.sub(r"\/+","/",re.sub(r"\\","/",p))), filepaths))
-    segmented_paths = [f.split("/") for f in filepaths]
-    root_node=TreeNode()
+    filepaths = list(
+        map(
+            lambda p: re.sub(
+                r"((^\/)|(\/$))", "", re.sub(r"\/+", "/", re.sub(r"\\", "/", p))
+            ),
+            filepaths,
+        )
+    )
+    segmented_paths = [smartsplit(f) for f in filepaths]
+    root_node = TreeNode()
     for segmented_path in segmented_paths:
         if len(segmented_path) == 0:
             continue
